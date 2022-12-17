c552ccc5f82fd26a48d42c9dd832bef7bfb1a88b
LPFreitas <lfreitasp2001@gmail.com>
2022-12-17 10:17:50 -0300

    FIX: identation

--- comp_do_mundo.vhd
+++ comp_do_mundo.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity comp_do_mundo is
-   port (
+    port (
         -- entradas
         clock              : in  std_logic;
         reset              : in  std_logic;
@@ -12,7 +12,7 @@ entity comp_do_mundo is
         bater_esquerda     : in  std_logic;
         echo               : in  std_logic;
         entrada_serial     : in  std_logic;
-		  config_displays    : in  std_logic;
+		config_displays    : in  std_logic;
         -- saidas
         pwm_goleiro        : out std_logic;
         pwm_batedor_dir    : out std_logic;
@@ -29,7 +29,7 @@ entity comp_do_mundo is
         hex2               : out std_logic_vector (6 downto 0);
         hex3               : out std_logic_vector (6 downto 0);
         hex4               : out std_logic_vector (6 downto 0);
-		  hex5               : out std_logic_vector (6 downto 0)
+		hex5               : out std_logic_vector (6 downto 0)
     );
 end entity;
 
@@ -61,32 +61,32 @@ architecture arch_comp_do_mundo of comp_do_mundo is
             db_gols_B          : out std_logic_vector (3 downto 0);
             db_rodada          : out std_logic_vector (3 downto 0);
             db_estado_uc       : out std_logic_vector (3 downto 0);
-				db_distancia       : out std_logic_vector (11 downto 0);
-				db_estado_detector : out std_logic_vector (3 downto 0)
+			db_distancia       : out std_logic_vector (11 downto 0);
+			db_estado_detector : out std_logic_vector (3 downto 0)
         );
     end component;
 	 
 	 component construtor_displays is
         port (
-		      -- selecao
-				config_displays       : in  std_logic;
-				-- entradas de debug (config = 0)
-				gols_A             : in  std_logic_vector(3 downto 0);
-				gols_B             : in  std_logic_vector(3 downto 0);
-				rodada             : in  std_logic_vector(3 downto 0);
-				estado_uc          : in  std_logic_vector(3 downto 0);
-				fim_jogo           : in  std_logic;
-				ganhador           : in  std_logic;
-				-- entradas de debug (config = 1)
-				distancia          : in  std_logic_vector (11 downto 0);
-			   detector_gol_uc    : in  std_logic_vector (3 downto 0);
-			   -- displays
-			   hex0                  : out std_logic_vector (6 downto 0);
-			   hex1                  : out std_logic_vector (6 downto 0);
-			   hex2                  : out std_logic_vector (6 downto 0);
-			   hex3                  : out std_logic_vector (6 downto 0);
-			   hex4                  : out std_logic_vector (6 downto 0);
-			   hex5                  : out std_logic_vector (6 downto 0)
+		    -- selecao
+			config_displays    : in  std_logic;
+			-- entradas de debug (config = 0)
+			gols_A             : in  std_logic_vector(3 downto 0);
+			gols_B             : in  std_logic_vector(3 downto 0);
+			rodada             : in  std_logic_vector(3 downto 0);
+			estado_uc          : in  std_logic_vector(3 downto 0);
+			fim_jogo           : in  std_logic;
+			ganhador           : in  std_logic;
+			-- entradas de debug (config = 1)
+			distancia          : in  std_logic_vector (11 downto 0);
+			detector_gol_uc    : in  std_logic_vector (3 downto 0);
+			-- displays
+			hex0               : out std_logic_vector (6 downto 0);
+			hex1               : out std_logic_vector (6 downto 0);
+			hex2               : out std_logic_vector (6 downto 0);
+			hex3               : out std_logic_vector (6 downto 0);
+			hex4               : out std_logic_vector (6 downto 0);
+			hex5               : out std_logic_vector (6 downto 0)
 	     );
 	 end component;
     
@@ -103,8 +103,8 @@ architecture arch_comp_do_mundo of comp_do_mundo is
     signal s_trigger, s_echo                                           : std_logic;
     signal s_fim_jogo, s_jogador, s_ganhador                           : std_logic;
     signal s_gols_A, s_gols_B, s_rodada, s_estado_uc : std_logic_vector (3 downto 0);
-	 signal s_db_estado_detector                      : std_logic_vector (3 downto 0);
-	 signal s_db_distancia                            : std_logic_vector (11 downto 0);
+	signal s_db_estado_detector                      : std_logic_vector (3 downto 0);
+	signal s_db_distancia                            : std_logic_vector (11 downto 0);
   
 begin
 
@@ -133,8 +133,8 @@ begin
             db_gols_B          => s_gols_B,
             db_rodada          => s_rodada,
             db_estado_uc       => s_estado_uc,
-				db_distancia       => s_db_distancia,
-				db_estado_detector => s_db_estado_detector
+			db_distancia       => s_db_distancia,
+			db_estado_detector => s_db_estado_detector
         );
 
     iniciar_detector: edge_detector
@@ -160,25 +160,25 @@ begin
 
 	 construtor_7seg: construtor_displays
         port map (
-		      -- selecao
-				config_displays => config_displays,
-				-- entradas de debug (config = 0)
-				gols_A             => s_gols_A,
-				gols_B             => s_gols_B,
-				rodada             => s_rodada,
-				estado_uc          => s_estado_uc,
-				fim_jogo           => s_fim_jogo,
-				ganhador           => s_ganhador,
-				-- entradas de debug (config = 1)
-				distancia          => s_db_distancia,
-			   detector_gol_uc    => s_db_estado_detector,
-			   -- displays
-			   hex0                => hex0,
-			   hex1                => hex1,
-			   hex2                => hex2,
-			   hex3                => hex3,
-			   hex4                => hex4,
-			   hex5                => hex5
+		    -- selecao
+			config_displays => config_displays,
+			-- entradas de debug (config = 0)
+			gols_A             => s_gols_A,
+			gols_B             => s_gols_B,
+			rodada             => s_rodada,
+			estado_uc          => s_estado_uc,
+			fim_jogo           => s_fim_jogo,
+			ganhador           => s_ganhador,
+			-- entradas de debug (config = 1)
+			distancia          => s_db_distancia,
+			detector_gol_uc    => s_db_estado_detector,
+			-- displays
+			hex0                => hex0,
+			hex1                => hex1,
+			hex2                => hex2,
+			hex3                => hex3,
+			hex4                => hex4,
+			hex5                => hex5
 	     );
 
     -- Pull up
