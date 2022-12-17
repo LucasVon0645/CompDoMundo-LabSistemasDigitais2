33e30e6aee2a752827674dd82d421294f97aaad3
LPFreitas <lfreitasp2001@gmail.com>
2022-12-17 10:18:01 -0300

    UPDATE: testbench

--- testbenchs/comp_do_mundo_tb.vhd
+++ testbenchs/comp_do_mundo_tb.vhd
@@ -6,17 +6,19 @@ end entity;
 
 architecture tb of comp_do_mundo_tb is
   
-  -- Componente a ser testado (Device Under Test -- DUT)
-  component comp_do_mundo
+    -- Componente a ser testado (Device Under Test -- DUT)
+
+    component comp_do_mundo is
         port (
             -- entradas
             clock              : in  std_logic;
             reset              : in  std_logic;
             iniciar            : in  std_logic;
-            posicao_batedor    : in  std_logic;
-            bater              : in  std_logic;
+            bater_direita      : in  std_logic;
+            bater_esquerda     : in  std_logic;
             echo               : in  std_logic;
             entrada_serial     : in  std_logic;
+            config_displays    : in  std_logic;
             -- saidas
             pwm_goleiro        : out std_logic;
             pwm_batedor_dir    : out std_logic;
@@ -26,35 +28,36 @@ architecture tb of comp_do_mundo_tb is
             -- depuracao
             db_fim_preparacao  : out std_logic;
             db_fim_transmissao : out std_logic;
-            db_ganhador        : out std_logic;
-            db_gols_A          : out std_logic_vector (6 downto 0);
-            db_gols_B          : out std_logic_vector (6 downto 0);
-            db_rodada          : out std_logic_vector (6 downto 0);
             db_trigger         : out std_logic;
             db_echo            : out std_logic;
-            db_estado          : out std_logic_vector (6 downto 0)
+            hex0               : out std_logic_vector (6 downto 0);
+            hex1               : out std_logic_vector (6 downto 0);
+            hex2               : out std_logic_vector (6 downto 0);
+            hex3               : out std_logic_vector (6 downto 0);
+            hex4               : out std_logic_vector (6 downto 0);
+            hex5               : out std_logic_vector (6 downto 0)
         );
     end component;
     
     -- Declaracao de sinais para conectar o componente a ser testado (DUT)
     --   valores iniciais para fins de simulacao (GHDL ou ModelSim)
-    signal clock_in            : std_logic := '0';
-    signal reset_in            : std_logic := '0';
-    signal iniciar_in          : std_logic := '0';
-    signal posicao_batedor_in  : std_logic := '0';
-    signal bater_in            : std_logic := '0';
-    signal echo_in             : std_logic := '0';
+    signal clock_in               : std_logic := '0';
+    signal reset_in               : std_logic := '0';
+    signal iniciar_in             : std_logic := '0';
+    signal bater_direita_in       : std_logic := '0';
+    signal bater_esquerda_in      : std_logic := '0';
+    signal echo_in                : std_logic := '0';
 
-    signal entrada_serial_in   : std_logic := '1';
-    signal serial_data         : std_logic_vector(7 downto 0) := "00000000";
+    signal entrada_serial_in      : std_logic := '1';
+    signal serial_data            : std_logic_vector(7 downto 0) := "00000000";
 
-    signal pwm_goleiro_out     : std_logic := '0';
-    signal pwm_batedor_dir_out : std_logic := '0';
-    signal pwm_batedor_esq_out : std_logic := '0';
-    signal trigger_out         : std_logic := '0';
-    signal saida_serial_out    : std_logic := '1';
-    signal fim_preparacao_out  : std_logic := '0';
-    signal fim_transmissao_out : std_logic := '0';
+    signal pwm_goleiro_out        : std_logic := '0';
+    signal pwm_batedor_dir_out    : std_logic := '0';
+    signal pwm_batedor_esq_out    : std_logic := '0';
+    signal trigger_out            : std_logic := '0';
+    signal saida_serial_out       : std_logic := '1';
+    signal db_fim_preparacao_out  : std_logic := '0';
+    signal db_fim_transmissao_out : std_logic := '0';
 
 
     -- Configuracoes do clock
@@ -102,25 +105,25 @@ architecture tb of comp_do_mundo_tb is
         -- 118us == 2cm de medicao ->  gooooooool
         -- 657us == 10cm de medicao -> nao ha gol
 
-        -- ( 1, 1, '0', "01100011", 118, '1', "01100011", 647), -- A: 1, B: 0 - A faz gol
-        -- ( 1, 2, '1', "01101001", 118, '0', "01101010", 647), -- A: 2, B: 0 - A faz gol
-        -- ( 1, 3, '1', "01100110", 118, '1', "01100101", 647), -- A: 3, B: 0 - A faz gol = A vence
+        ( 1, 1, '0', "01100011", 118, '1', "01100011", 647), -- A: 1, B: 0 - A faz gol
+        ( 1, 2, '1', "01101001", 118, '0', "01101010", 647), -- A: 2, B: 0 - A faz gol
+        ( 1, 3, '1', "01100110", 118, '1', "01100101", 647), -- A: 3, B: 0 - A faz gol = A vence
 
-        -- ( 2, 1, '1', "01101010", 118, '0', "01101010", 647), -- A: 1, B: 0 - A faz gol
-        -- ( 2, 2, '1', "01100101", 118, '1', "01101010", 118), -- A: 2, B: 1 - A e B fazem gol
-        -- ( 2, 3, '0', "01100110", 118, '0', "01101001", 647), -- A: 3, B: 1 - A faz gol
-        -- ( 2, 4, '0', "01100011", 118, '0', "00000000", 0),   -- A: 4, B: 1 - A faz gol = A vence
+        ( 2, 1, '1', "01101010", 118, '0', "01101010", 647), -- A: 1, B: 0 - A faz gol
+        ( 2, 2, '1', "01100101", 118, '1', "01101010", 118), -- A: 2, B: 1 - A e B fazem gol
+        ( 2, 3, '0', "01100110", 118, '0', "01101001", 647), -- A: 3, B: 1 - A faz gol
+        ( 2, 4, '0', "01100011", 118, '0', "00000000", 0),   -- A: 4, B: 1 - A faz gol = A vence
 
-        -- ( 3, 1, '0', "01101001", 647, '1', "01100101", 647), -- A: 0, B: 0 - Ninguem faz gol
-        -- ( 3, 2, '1', "01100101", 647, '0', "01101010", 647), -- A: 0, B: 0 - Ninguem faz gol
-        -- ( 3, 3, '1', "01100011", 647, '1', "01101001", 118), -- A: 0, B: 1 - B faz gol
-        -- ( 3, 4, '0', "01101011", 647, '0', "01100110", 118), -- A: 0, B: 2 - B faz gol = B vence
+        ( 3, 1, '0', "01101001", 647, '1', "01100101", 647), -- A: 0, B: 0 - Ninguem faz gol
+        ( 3, 2, '1', "01100101", 647, '0', "01101010", 647), -- A: 0, B: 0 - Ninguem faz gol
+        ( 3, 3, '1', "01100011", 647, '1', "01101001", 118), -- A: 0, B: 1 - B faz gol
+        ( 3, 4, '0', "01101011", 647, '0', "01100110", 118), -- A: 0, B: 2 - B faz gol = B vence
 
-        -- ( 4, 1, '1', "01100101", 647, '0', "01100011", 118), -- A: 0, B: 1 - B faz gol
-        -- ( 4, 2, '1', "01100011", 647, '1', "01100110", 118), -- A: 0, B: 2 - B faz gol
-        -- ( 4, 3, '0', "01100110", 118, '1', "01101010", 118), -- A: 1, B: 3 - A e B fazem gol
-        -- ( 4, 4, '0', "01101001", 118, '1', "01100101", 647), -- A: 2, B: 3 - A faz gol
-        -- ( 4, 5, '1', "01100011", 647, '0', "00000000", 0),   -- A: 2, B: 3 - Ninguem faz gol = B vence
+        ( 4, 1, '1', "01100101", 647, '0', "01100011", 118), -- A: 0, B: 1 - B faz gol
+        ( 4, 2, '1', "01100011", 647, '1', "01100110", 118), -- A: 0, B: 2 - B faz gol
+        ( 4, 3, '0', "01100110", 118, '1', "01101010", 118), -- A: 1, B: 3 - A e B fazem gol
+        ( 4, 4, '0', "01101001", 118, '1', "01100101", 647), -- A: 2, B: 3 - A faz gol
+        ( 4, 5, '1', "01100011", 647, '0', "00000000", 0),   -- A: 2, B: 3 - Ninguem faz gol = B vence
 
         ( 5, 1, '0', "01100101", 118, '1', "01100101", 647), -- A: 1, B: 0 - A faz gol
         ( 5, 2, '0', "01101010", 647, '0', "01100011", 118), -- A: 1, B: 1 - B faz gol
@@ -163,10 +166,11 @@ begin
             clock              => clock_in,
             reset              => reset_in,
             iniciar            => iniciar_in,
-            posicao_batedor    => posicao_batedor_in,
-            bater              => bater_in,
+            bater_direita      => bater_direita_in,
+            bater_esquerda     => bater_esquerda_in,
             echo               => echo_in,
             entrada_serial     => entrada_serial_in,
+            config_displays    => '0',
             -- saidas
             pwm_goleiro        => pwm_goleiro_out,
             pwm_batedor_dir    => pwm_batedor_dir_out,
@@ -174,15 +178,16 @@ begin
             trigger            => trigger_out,
             saida_serial       => saida_serial_out,
             -- depuracao
-            db_fim_preparacao  => fim_preparacao_out,
-            db_fim_transmissao => fim_transmissao_out,
-            db_ganhador        => open,
-            db_estado          => open,
-            db_gols_A          => open,
-            db_gols_B          => open,
-            db_rodada          => open,
+            db_fim_preparacao  => db_fim_preparacao_out,
+            db_fim_transmissao => db_fim_transmissao_out,
             db_trigger         => open,
-            db_echo            => open
+            db_echo            => open,
+            hex0               => open,
+            hex1               => open,
+            hex2               => open,
+            hex3               => open,
+            hex4               => open,
+            hex5               => open
         );
 
     -- geracao dos sinais de entrada (estimulos)
@@ -203,7 +208,7 @@ begin
         reset_in <= '0';
         wait until falling_edge(clock_in);
 
-        wait until fim_transmissao_out = '1'; -- espera transmitir inicio
+        wait until db_fim_transmissao_out = '1'; -- espera transmitir inicio
 
         ---- loop pelas posicoes de teste ----
         for i in posicoes_teste'range loop
@@ -214,16 +219,16 @@ begin
             severity note;
 
             if posicoes_teste(i).rodada = 1 then
-            -- aciona iniciar
-            wait for 1 us;
-            iniciar_in <= '1';
-            wait for 1 us;
-            iniciar_in <= '0';
+                -- aciona iniciar
+                wait for 1 us;
+                iniciar_in <= '1';
+                wait for 1 us;
+                iniciar_in <= '0';
             end if;
 
             -- Jogada de A
 
-            wait until fim_transmissao_out = '1'; -- espera transmitir preparacao
+            wait until db_fim_transmissao_out = '1'; -- espera transmitir preparacao
 
             -- envia dadoB
             serial_data <= posicoes_teste(i).dadoB;
@@ -232,16 +237,23 @@ begin
             entrada_serial_in <= '1'; -- repouso
             wait for bitPeriod;
 
-            -- wait until fim_preparacao_out = '1'; -- espera fim de preparacao
+            -- wait until db_fim_preparacao_out = '1'; -- espera fim de preparacao
 
-            wait until fim_transmissao_out = '1'; -- espera transmitir batedor
-
-            posicao_batedor_in <= posicoes_teste(i).posicaoA;
-
-            wait for 1 us;
-            bater_in <= '1';
-            wait for 1 us;
-            bater_in <= '0';
+            wait until db_fim_transmissao_out = '1'; -- espera transmitir batedor
+            
+            if posicoes_teste(i).posicaoA = '0' then
+                -- bater direita
+                wait for 1 us;
+                bater_direita_in <= '1';
+                wait for 1 us;
+                bater_direita_in <= '0';
+            else
+                -- bater esquerda
+                wait for 1 us;
+                bater_esquerda_in <= '1';
+                wait for 1 us;
+                bater_esquerda_in <= '0';
+            end if;
 
             -- determina largura do pulso echo para A
             larguraPulsoA <= posicoes_teste(i).tempoA * 1 us;
@@ -257,28 +269,35 @@ begin
             wait for larguraPulsoA;
             echo_in <= '0';
 
-            wait until fim_transmissao_out = '1'; -- espera transmitir informacoes
+            wait until db_fim_transmissao_out = '1'; -- espera transmitir informacoes
             
             -- Jogada de B
             
             if posicoes_teste(i).tempoB /= 0 then
-            wait until fim_transmissao_out = '1'; -- espera transmitir preparacao
+                wait until db_fim_transmissao_out = '1'; -- espera transmitir preparacao
 
-            -- envia dadoB
-            serial_data <= posicoes_teste(i).dadoA;
-            wait for 2*bitPeriod; -- aguarda 2 periodos de bit antes de enviar
-            UART_WRITE_BYTE ( Data_In=>serial_data, Serial_Out=>entrada_serial_in );
-            entrada_serial_in <= '1'; -- repouso
+                -- envia dadoB
+                serial_data <= posicoes_teste(i).dadoA;
+                wait for 2*bitPeriod; -- aguarda 2 periodos de bit antes de enviar
+                UART_WRITE_BYTE ( Data_In=>serial_data, Serial_Out=>entrada_serial_in );
+                entrada_serial_in <= '1'; -- repouso
             wait for bitPeriod;
 
-            wait until fim_transmissao_out = '1'; -- espera transmitir batedor
+            wait until db_fim_transmissao_out = '1'; -- espera transmitir batedor
 
-            posicao_batedor_in <= posicoes_teste(i).posicaoB;
-
-            wait for 1 us;
-            bater_in <= '1';
-            wait for 1 us;
-            bater_in <= '0';
+            if posicoes_teste(i).posicaoB = '0' then
+                -- bater direita
+                wait for 1 us;
+                bater_direita_in <= '1';
+                wait for 1 us;
+                bater_direita_in <= '0';
+            else
+                -- bater esquerda
+                wait for 1 us;
+                bater_esquerda_in <= '1';
+                wait for 1 us;
+                bater_esquerda_in <= '0';
+            end if;
 
             -- determina largura do pulso echo para A
             larguraPulsoB <= posicoes_teste(i).tempoB * 1 us;
@@ -294,7 +313,7 @@ begin
             wait for larguraPulsoB;
             echo_in <= '0';
 
-            wait until fim_transmissao_out = '1'; -- espera transmitir informacoes
+            wait until db_fim_transmissao_out = '1'; -- espera transmitir informacoes
             end if;
         end loop;
 
